7de21cb094c1c76116513a1caf1a362dca6e16be
ssdh233 <songsongdahu233@gmail.com>
2023-08-05 18:13:17 +0900

    Use classic/retail folder for different client version

---
+++ retail/dict.lua
@@ -0,0 +1,29 @@
+DictSourceCN1={}
+DictSourceCN2={}
+DictSourceEN={}
+DictSourceJA={}
+
+DictSource = {
+  ["en"] = {
+    ["data"]=DictSourceEN,
+    ["noResultText"]="No result.",
+    ["source"]="source: New Oxford American Dictionary"
+  },
+  ["cn1"] = {
+    ["data"]=DictSourceCN1,
+    ["noResultText"]="没有找到结果。",
+    ["source"]="来源: 牛津英汉汉英词典"
+  },
+  ["cn2"] = {
+    ["data"]=DictSourceCN2,
+    ["noResultText"]="没有找到结果。",
+    ["source"]="来源: ECDICT"
+  },
+  ["ja"] = {
+    ["data"]=DictSourceJA,
+    ["noResultText"]="結果が見つかりませんでした。",
+    ["source"]="出典: ejdict-hand"
+  }
+}
+
+DictKeys = {"cn1", "en", "cn2", "ja"}
